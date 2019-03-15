de39d062495f28c13fc027b8c5a2b82824bbac5c
Francis Avila <favila@breezeehr.com>
2019-03-15 18:42:24 -0500

    Stop using lein-tools-deps for packaging
    Lein-tools-deps will flatten all dependencies, which makes it
    unsuitable for packaging a library jar. Just use lein directly for now.

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 (defproject com.breezeehr/hitch2-transit-cljs "0.3.1-SNAPSHOT"
   :description ""
-  ;; lein-tools-deps plugin does not work with lein 2.7.1
-  :min-lein-version "2.8.0"
   :url "https://github.com/Breezeemr/hitch2/tree/master/"
   :repositories [["snapshots" {:url "s3p://breezepackages/snapshots" :creds :gpg}]
                  ["releases" {:url "s3p://breezepackages/releases" :creds :gpg}]]
-  :plugins [[lein-tools-deps "0.4.1"]]
-  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]})
+  :dependencies [[org.clojure/clojurescript "1.10.339"]
+                 [com.cognitect/transit-cljs "0.8.256"]
+                 [com.breezeehr/hitch2 "0.3.2-SNAPSHOT"]])
